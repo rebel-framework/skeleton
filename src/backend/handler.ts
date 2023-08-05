67db2b14e1d13530dbb625759ad68c66abc3ee2a
notu <notuart@pm.me>
2023-08-05 23:42:54 +0100

    Cleaning things up

--- src/backend/handler.ts
+++ src/backend/handler.ts
@@ -3,11 +3,6 @@ import { Request, Context, response, errorHandler } from '@rebel/core';
 import router from './routes';
 
 export async function handler(request: Request, context: Context) {
-  // console.log('ROUTES: \\n' + JSON.stringify(router.routes, null, 2));
-  // console.log('REQUEST: \\n' + JSON.stringify(request, null, 2));
-  // console.log('CONTEXT: \\n' + JSON.stringify(context, null, 2));
-  console.log('UNSERIALIZED', { unserialzied: 'data' });
-
   try {
     return await router.handle(request, context);
   } catch (error) {
