efed75f47059afaa9e301f2cf93ec9299174795d
notu <notuart@pm.me>
2023-08-07 16:27:37 +0100

    Add comments to handler.ts

--- src/backend/handler.ts
+++ src/backend/handler.ts
@@ -1,8 +1,19 @@
-import { Request, Context, errorHandler } from '@rebel/core';
+import { Request, Response, Context, errorHandler } from '@rebel/core';
 
 import router from './routes';
 
-export async function handler(request: Request, context: Context) {
+/**
+ * Mono lambda handler for the whole project.
+ * Comes with error handling out of the box.
+ *
+ * @param {Request} request
+ * @param {Context} context
+ * @returns {Response}
+ */
+export async function handler(
+  request: Request,
+  context: Context
+): Promise<Response> {
   try {
     return await router.handle(request, context);
   } catch (error) {
